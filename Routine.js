64b16b577f649f0d712a198d9f2634d3a1e612b8
Fatoumata DIARRA <dfatoumata57@yahoo.fr>
2021-08-25 09:56:55 +0200

    Initialize project using Create React App

---
+++ Routine.js
@@ -0,0 +1,11 @@
+function Routine() {
+  return (
+    <div>
+      <button onClick={alert("Button Manger clicked")}> Manger </button>
+      <button onClick={alert("Button Dormir clicked")}> Dormir </button>
+      <button onClick={alert("Button Etudier clicked")}> Etudier </button>
+    </div>
+  );
+}
+
+export default Routine;
